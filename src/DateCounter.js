b60541ea3f9803f9eade4ce7ee04761bca8561e7
karlijones <karlijones1984@gmail.com>
2025-01-29 14:23:29 -0600

    if statement for action.type

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,7 +2,9 @@ import { useState, useReducer } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  return state + action;
+  if(action.type === "inc") return state + action.payload;
+  if(action.type === "dec") return state - action.payload;
+  if(action.type === "setCount") return action.payload;
   }
 
 function DateCounter() {
@@ -17,13 +19,13 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch(-1);
+    dispatch({type: "dec", payload: -1});
     // setCount((count) => count - 1);
     //setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch(1);
+    dispatch({type: "inc", payload: 1});
     // setCount((count) => count + 1);
     //setCount((count) => count + step);
   };
