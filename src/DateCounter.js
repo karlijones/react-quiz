e848450889a82f0e521b250f72c29cdfaf84a5c4
karlijones <karlijones1984@gmail.com>
2025-01-31 16:41:26 -0600

    switch state in reducer function

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,10 +2,19 @@ import { useReducer } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  if(action.type === "inc") return state + 1;
-  if(action.type === "dec") return state - 1;
-  if(action.type === "setCount") return action.payload;
+
+  return { count: 0, step: 1 };
+
+  switch( action.type ) {
+    case "dec":
+      return { ...state, count: state.count + 1};
   }
+    
+
+  //if(action.type === "inc") return state + 1;
+  //if(action.type === "dec") return state - 1;
+  //if(action.type === "setCount") return action.payload;
+}
 
 function DateCounter() {
   //const [count, setCount] = useState(0);
