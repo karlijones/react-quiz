326ffd11e46441f125b06b18e6bb40f54f9908f8
karlijones <karlijones1984@gmail.com>
2025-02-17 20:41:30 -0600

    main element added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ export default function App() {
   return (
     <div className="app">
       <Header />
+
+      <main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </main>
     </div>
   );
 }
