f4af6d710d852a6fc1cc8e7dd40f0a29210ab858
karlijones <karlijones1984@gmail.com>
2025-02-23 21:20:16 -0600

    created children prop and imported Main to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Header from './Header';
+import Main from './Main';
 
 export default function App() {
   return (
     <div className="app">
       <Header />
 
-      <main className="main">
+      <Main>
         <p>1/15</p>
         <p>Question?</p>
-      </main>
+      </Main>
     </div>
   );
 }
